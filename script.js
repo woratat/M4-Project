2cdc891b51ee6c0aa6a3c2203a3e16a6770af994
woratat <84006033+woratat@users.noreply.github.com>
2021-06-21 22:14:26 +0700

    5.Add delete button
    ปุ่มลบและแก้ไข pop up

--- script.js
+++ script.js
@@ -4,9 +4,12 @@ const id = document.getElementById('id')
 const title = document.getElementById('title')
 const type = document.getElementById('type')
 const episodes = document.getElementById('episodes')
+const synopsis = document.getElementById('synopsis')
 const score = document.getElementById('score')
 const rated = document.getElementById('rated')
-const image= document.getElementById('image')
+const image = document.getElementById('image')
+const displayMyMovie = document.getElementById('displayMyMovie')
+const deleteButton = document.getElementById('deleteButton')
 
 window.addEventListener('load',(e)=>{
     const searchButton = document.getElementById('searchButton')
@@ -102,33 +105,113 @@ function addMyList(myMovie){
         return null
     })
 }
+function hideAll(){
+    search_results.style.display='none'
+}
 document.getElementById('MyList').addEventListener('click',(e)=>{
     display()
 })
 function display(){
-    
+    hideAll()
+    displayMyMovie.style.display='flex'
+    displayMyMovie.style.flexFlow='row wrap'
+    displayMyMovie.style.justifyContent='center'
+    getMyMovie()
 }
-
-// document.getElementById()
-// function getOneCard(id){
-//     fetch(`https://api.jikan.moe/v3/anime/${id}`, {
-//         method: 'GET'
-//     }).then((res) => {
-//         if (res.status === 200) {
-//             return res.json();
-//         }
-//     }).then((data) => {
-//         showModal(data.results);
-//     })
-// }
-// function showModal(movie){
-//     modalTitle.innerHTML = movie.title
-//     id.innerHTML = movie.mal_id
-//     title.innerHTML = movie.title
-//     type.innerHTML = movie.type
-//     episodes.innerHTML = movie.episodes
-//     score.innerHTML = movie.score
-//     rated.innerHTML = movie.rated
-//     image.setAttribute('src',movie.image)
-//     image.setAttribute('width',"304")
-// }
+function displayMyCard(movie){
+    let card = document.createElement('div')
+    card.classList.add('card')
+    card.classList.add('bg-dark')
+    card.classList.add('text-white')
+    card.classList.add('mt-3')
+    card.classList.add('mx-3')
+    card.setAttribute('style','width: 18rem;')
+    card.setAttribute('id',movie.id)
+    let img = document.createElement('img')
+    img.setAttribute('src',movie.image_url)
+    img.classList.add('card-img-top')
+    let card_body = document.createElement('div')
+    card_body.classList.add('card-body')
+    let title = document.createElement('h5')
+    title.classList.add('card-title')
+    title.innerText = movie.title
+    let synop = document.createElement('p')
+    synop.classList.add('card-text')
+    synop.innerHTML = movie.synopsis
+    let button = document.createElement('button')
+    button.classList.add('btn')
+    button.classList.add('btn-outline-success')
+    button.setAttribute('type','button')
+    button.setAttribute('id','moreInfo')
+    button.setAttribute('data-bs-toggle','modal')
+    button.setAttribute('data-bs-target','#myModal')
+    button.innerText = "More"
+    button.addEventListener('click',(e)=>{
+        getOneCard(movie.id)
+    })
+    card_body.appendChild(title)
+    card_body.appendChild(synop)
+    card_body.appendChild(button)
+    card.appendChild(img)
+    card.appendChild(card_body)
+    displayMyMovie.appendChild(card)
+}
+function getMyMovie(){
+    fetch(`https://se104-project-backend.du.r.appspot.com/movies/632110352`, {
+        method: 'GET'
+    }).then((res) => {
+        if (res.status === 200) {
+            return res.json();
+        }
+    }).then((data) => {
+        showMyMovie(data);
+    })
+}
+function showMyMovie(data){
+    for(movie of data){
+        displayMyCard(movie)
+    }
+}
+function getOneCard(id){
+    fetch(`https://se104-project-backend.du.r.appspot.com/movie/632110352/${id}`, {
+        method: 'GET'
+    }).then((res) => {
+        if (res.status === 200) {
+            return res.json();
+        }
+    }).then((data) => {
+        showModal(data);
+    })
+}
+function showModal(movie){
+    modalTitle.innerHTML = movie.title
+    title.innerHTML = movie.title
+    synopsis.innerHTML = movie.synopsis
+    type.innerHTML = movie.type
+    episodes.innerHTML = movie.episodes
+    score.innerHTML = movie.score
+    rated.innerHTML = movie.rated
+    image.setAttribute('src',movie.image_url)
+    deleteButton.addEventListener('click',(e)=>{
+        let confirmMsg = confirm(`ต้องการลบ ${movie.title} ใช่หรือไม่`)
+            if(confirmMsg){
+                deleteList(movie.id)
+            }
+    })
+}
+function deleteList(id){
+    fetch(`https://se104-project-backend.du.r.appspot.com/movie?id=632110352&&movieId=${id}`,{
+        method: 'DELETE',
+    }).then(response =>{
+        if(response.status===200){
+            return response.json()
+        }else{
+            throw Error(response.statusText)
+        }
+    }).then(data=>{
+        alert(`${data.title} is now deleted`)
+        location.reload()
+    }).catch(error=>{
+        alert('This is not in the database')
+    })
+}
