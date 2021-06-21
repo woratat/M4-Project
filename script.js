fda9eb70bfb822f5f3e0859acdf974bb7c18df7f
woratat <84006033+woratat@users.noreply.github.com>
2021-06-21 15:16:14 +0700

    2.Find Bug
    หาบัค

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
-window.addEventListener('load', (e) => {
-    const button = document.getElementById('searchButton')
-    button.addEventListener('click', (e)=>{
-        getAllMovie()
+const search_results = document.getElementById('search-results')
+const modalTitle = document.getElementById('modalTitle')
+const id = document.getElementById('id')
+const title = document.getElementById('title')
+const type = document.getElementById('type')
+const episodes = document.getElementById('episodes')
+const score = document.getElementById('score')
+const rated = document.getElementById('rated')
+const image= document.getElementById('image')
+
+window.addEventListener('load',(e)=>{
+    const searchButton = document.getElementById('searchButton')
+    searchButton.addEventListener('click', (e)=>{
+        const search = document.getElementById('search').value
+        getAllMovie(search)
+        console.log(search)
     })
-});
-function getAllMovie(){
-    fetch(`https://api.jikan.moe/v3/search/anime?q=&order_by=members&sort=desc&page=1`, {
+})
+
+// searchButton.addEventListener('click', (e)=>{
+//     const search = document.getElementById('search').value
+//     getAllMovie(search)
+//     console.log(search)
+// })
+function getAllMovie(search){
+    fetch(`https://api.jikan.moe/v3/search/anime?q=${search}`, {
         method: 'GET'
     }).then((res) => {
         if (res.status === 200) {
@@ -15,7 +33,6 @@ function getAllMovie(){
         showMovie(data.results);
     console.log('this is getAllMovie')
     })
-
 }
 function showMovie(data){
     console.log('this is showMovie')
@@ -23,12 +40,11 @@ function showMovie(data){
         showCard(movie)
     }
 }
-
-const search_results = document.getElementById('search-results')
-
 function showCard(movie){
     let card = document.createElement('div')
     card.classList.add('card')
+    card.classList.add('bg-dark')
+    card.classList.add('text-white')
     card.classList.add('mt-3')
     card.classList.add('mx-3')
     card.setAttribute('style','width: 18rem;')
@@ -39,15 +55,22 @@ function showCard(movie){
     card_body.classList.add('card-body')
     let title = document.createElement('h5')
     title.classList.add('card-title')
-    title.innerHTML = movie.title
+    title.innerText = movie.title
     let synop = document.createElement('p')
     synop.classList.add('card-text')
     synop.innerHTML = movie.synopsis
     let more = document.createElement('a')
     more.setAttribute('href','#')
     more.classList.add('btn')
-    more.classList.add('btn-primary')
-    more.innerHTML = 'Go somewhere'
+    more.classList.add('text-danger')
+    more.classList.add('btn-dark')
+    // more.setAttribute('data-bs-toggle','modal')
+    // more.setAttribute('data-bs-target','#exampleModal')
+    more.innerText = 'Add to list'
+    // more.addEventListener('click',(event)=>{
+    //     console.log('click more'+movie.mal_id)
+    //     getOneCard(movie.mal_id)
+    // })
     card_body.appendChild(title)
     card_body.appendChild(synop)
     card_body.appendChild(more)
@@ -56,4 +79,25 @@ function showCard(movie){
     search_results.appendChild(card)
     console.log('this is the end')
 }
-
+// function getOneCard(id){
+//     fetch(`https://api.jikan.moe/v3/anime/${id}`, {
+//         method: 'GET'
+//     }).then((res) => {
+//         if (res.status === 200) {
+//             return res.json();
+//         }
+//     }).then((data) => {
+//         showModal(data.results);
+//     })
+// }
+// function showModal(movie){
+//     modalTitle.innerHTML = movie.title
+//     id.innerHTML = movie.mal_id
+//     title.innerHTML = movie.title
+//     type.innerHTML = movie.type
+//     episodes.innerHTML = movie.episodes
+//     score.innerHTML = movie.score
+//     rated.innerHTML = movie.rated
+//     image.setAttribute('src',movie.image)
+//     image.setAttribute('width',"304")
+// }
